aa878b7f781f7c53196314c5fe291043dc847775
Plamen Todorov <pl.todorov@gmail.com>
2012-10-14 18:59:38 +0300

    twitter button

--- js/index.js
+++ js/index.js
@@ -4,12 +4,7 @@ $(document).ready(function() {
 	$('#manifestoCarousel').carousel('pause');
 	
 	!function(d,s,id){
-		console.log(d);
-		console.log(s);
-		console.log(id);
 		var js,fjs=d.getElementsByTagName(s)[0];
-		console.log(js);
-		console.log(fjs);
 		if(!d.getElementById(id)){
 			console.log("creating element");
 			js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";
