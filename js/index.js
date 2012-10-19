d330b4e51f9c3c385c0a0b0e4a604fcb660ac9e6
Plamen Todorov <pl.todorov@gmail.com>
2012-10-19 07:07:55 +0300

    g+ button

--- js/index.js
+++ js/index.js
@@ -1,14 +1,28 @@
 $(document).ready(function() {
 	$('.state').tooltip();
-	$('#manifestoCarousel').carousel(0)
-	$('#manifestoCarousel').carousel('pause');
+	$('#manifestoCarousel').carousel(0);
+	$('#manifestoCarousel').carousel('pause'); 
 	
-	!function(d,s,id){
-		var js,fjs=d.getElementsByTagName(s)[0];
-		if(!d.getElementById(id)){
+	//Twitter Button
+	! function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if(!d.getElementById(id)) {
 			console.log("creating element");
-			js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";
-			fjs.parentNode.insertBefore(js,fjs);
+			js = d.createElement(s);
+			js.id = id;
+			js.src = "//platform.twitter.com/widgets.js";
+			fjs.parentNode.insertBefore(js, fjs);
 		}
-	}(document,"script","twitter-wjs");
+	}(document, "script", "twitter-wjs"); 
+	
+	//G+ Button
+	(function() {
+		var po = document.createElement('script');
+		po.type = 'text/javascript';
+		po.async = true;
+		po.src = 'https://apis.google.com/js/plusone.js';
+		var s = document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(po, s);
+	})();
+
 });
